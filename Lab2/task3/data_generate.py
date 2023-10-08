2e28972df6210af74d03726208fb8a433c4340ac
Desxiaofeng <92841465+Desxiaofeng@users.noreply.github.com>
2023-10-08 17:06:53 +0800

    fix but

--- Lab2/task3/data_generate.py
+++ Lab2/task3/data_generate.py
@@ -11,6 +11,7 @@ for i in range(25600):
     #随机生成明文对
     input_data=random.randint(0,2**64-1)
     diff=0x4008000004000000
+    
     input_data_friend=input_data^diff
 
     #将数字明文转换成字符串明文，以16进制表示，共有16个16进制数(4bit)，总共64bit
@@ -25,8 +26,8 @@ for i in range(25600):
     command=['./des_enc1',plain_text[0], plain_text[1], plain_text[2], plain_text[3], plain_text[4], plain_text[5], plain_text[6], plain_text[7]]
     command_friend=['./des_enc1',plain_text_friend[0], plain_text_friend[1], plain_text_friend[2], plain_text_friend[3], plain_text_friend[4], plain_text_friend[5], plain_text_friend[6], plain_text_friend[7]]
     cipher = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
-    cipher_friend = subprocess.check_output(command, stderr=subprocess.STDOUT, text=True)
-    
+    cipher_friend = subprocess.check_output(command_friend, stderr=subprocess.STDOUT, text=True)
+
     #构造存储单元
     cipher_l=int(cipher[:8],16)
     cipher_r=int(cipher[8:],16)
@@ -38,7 +39,7 @@ for i in range(25600):
     
     cipher_pair=(cipher, cipher_friend)
     cipher_pair_list.append(cipher_pair)
-
+    
 #保存list对象，用于协同工作
 with open('cipher_pair_list.json','w') as file:
     json.dump(cipher_pair_list, file)
